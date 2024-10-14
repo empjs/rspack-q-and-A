a6ce7c803045fc4b6b0e6698abfc4707e86c86d4
liangxiaoming <liangxiaoming@yy.com>
2024-10-14 10:48:09 +0800

    feat: add demo

---
+++ projects/runtime_demo/host/src/component/ShareView/index.tsx
@@ -0,0 +1,11 @@
+import sm from './index.module.scss'
+
+const ShareView = () => {
+    return (
+        <div className={sm.wrap}>
+            <h1>ShareView</h1>
+        </div>
+    )
+}
+
+export default ShareView
